203058755d4829c109ac94c69629464358b86187
Alejae1998 <alejagr1998@gmail.com>
2022-10-27 11:58:49 -0700

    console.log the submit btn

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 /* Imports */
-
+const submitBtn = document.getElementById('submit');
 /* Get DOM Elements */
 
 /* State */
 
 /* Events */
-
+submitBtn.addEventListener('click', () => {
+    console.log('clicking submit');
+});
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
